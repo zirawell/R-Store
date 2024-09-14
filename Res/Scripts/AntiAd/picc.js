5167f996c6e0ae054ed213594be0d6829c4b5d5c
Dark Wang <zirawell@gmail.com>
2024-09-14 18:10:23 +0800

    upd picc

---
+++ Res/Scripts/AntiAd/picc.js
@@ -0,0 +1,28 @@
+/********************************
+Picc Remove Ads - Version 1.0
+Please note that you may need to reinstall app for script to work.
+
+QuantumultX rewrite link:
+https://raw.githubusercontent.com/zirawell/R-Store/main/Rule/QuanX/Adblock/App/Z/中国人保/rewrite/picc.conf
+
+********************************/
+
+const url = $request.url;
+if (!$response.body) $done({});
+let body = $response.body;
+let obj = JSON.parse(body);
+
+const actions = {
+  'homeInit': () => {
+    obj.data.startupPage = {};
+    obj.data.templates = obj.data.templates.filter(e => !e.name?.match(/主轮播图|保险推荐|专享|腰封轮播图|浮标配置|二楼营销位|首页主题/));
+  },
+  'myPageConfigList': () => {
+    obj.data.YFList = [];
+  }
+};
+
+const actionKey = Object.keys(actions).find(key => url.includes(key));
+actions[actionKey]?.();
+
+$done({body: JSON.stringify(obj)});
