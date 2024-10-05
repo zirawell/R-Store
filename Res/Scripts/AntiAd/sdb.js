f9a589130c139e4e6c67ad1e2b12096dd7bbfdb5
Dark Wang <zirawell@gmail.com>
2024-10-05 14:32:09 +0800

    upd sdb

---
+++ Res/Scripts/AntiAd/sdb.js
@@ -0,0 +1,20 @@
+/********************************
+SDB Remove Ads - Version 1.0
+Please note that you may need to reinstall app for script to work.
+
+QuantumultX rewrite link:
+https://raw.githubusercontent.com/zirawell/R-Store/main/Rule/QuanX/Adblock/App/P/平安口袋银行/rewrite/sdb.conf
+
+********************************/
+
+const url = $request.url;
+const header = $response.headers;
+
+const contType = header["Content-Type"];
+const contLength = header["Content-Length"];
+
+if (contType == "image/jpeg" || contType == "image/jpg" || contLength > 100000){
+  $done({ body:"", headers:"", status: "HTTP/1.1 204 No Content" });
+} else{
+  $done({});
+}
