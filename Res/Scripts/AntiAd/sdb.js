70bc5454e37c80c8c4843caca349f4d708af351b
Dark Wang <zirawell@gmail.com>
2024-10-06 19:16:45 +0800

    upd ehaier

--- Res/Scripts/AntiAd/sdb.js
+++ Res/Scripts/AntiAd/sdb.js
@@ -15,6 +15,4 @@ const contLength = header["Content-Length"];
 
 if (contType == "image/jpeg" || contType == "image/jpg" || contLength > 100000){
   $done({ body:"", headers:"", status: "HTTP/1.1 204 No Content" });
-} else{
-  $done({});
 }
