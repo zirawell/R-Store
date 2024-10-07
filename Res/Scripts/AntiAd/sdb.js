6f50819954a8cb4b792e1e42ca86b7f60dbf43d7
Dark Wang <zirawell@gmail.com>
2024-10-07 08:56:27 +0800

    upd sdb

--- Res/Scripts/AntiAd/sdb.js
+++ Res/Scripts/AntiAd/sdb.js
@@ -13,7 +13,7 @@ const header = $response.headers;
 const contType = header["Content-Type"];
 const contLength = header["Content-Length"];
 
-if (contType == "image/jpeg" || contType == "image/jpg" || contLength > 100000){
+if ((contType == "image/jpeg" || contType == "image/jpg") && contLength > 100000){
   $done({ body:"", headers:"", status: "HTTP/1.1 204 No Content" });
 }else{
   $done({});
