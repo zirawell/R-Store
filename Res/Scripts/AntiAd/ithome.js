165ee29ed74f26f72660711574d7e07e149f83c6
Dark Wang <zirawell@gmail.com>
2024-10-21 08:09:05 +0800

    upd ithome

---
+++ Res/Scripts/AntiAd/ithome.js
@@ -0,0 +1,37 @@
+/********************************
+ItHome Remove Ads - Version 1.0
+Checkout Source - https://raw.githubusercontent.com/RuCu6/QuanX/main/Scripts/ithome.js
+Please note that you may need to reinstall app for script to work.
+
+QuantumultX rewrite link:
+https://raw.githubusercontent.com/zirawell/R-Store/main/Rule/QuanX/Adblock/App/I/IT之家/rewrite/ithome.conf
+
+********************************/
+
+const url = $request.url;
+if (!$response.body) $done({});
+let obj = JSON.parse($response.body);
+
+if (
+  url.includes("/api/news/index") ||
+  url.includes("/api/topmenu/getfeeds")
+) {
+  if (obj?.data?.list?.length > 0) {
+    let list = obj.data.list;
+    const newList = [];
+    for (let item of list) {
+      if (item?.feedContent?.smallTags?.some((i) =>
+        i?.text?.includes("广告"))
+      ) {
+        continue;
+      }
+      if ([10002, 10003].includes(item?.feedType)) {
+        continue;
+      }
+      newList.push(item);
+    }
+    obj.data.list = newList;
+  }
+}
+
+$done({ body: JSON.stringify(obj) });
