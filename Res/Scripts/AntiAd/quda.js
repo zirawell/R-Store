68dfa0686a1ef30005ea423c8dc28711b77c4038
Dark Wang <zirawell@gmail.com>
2024-10-22 17:05:47 +0800

    upd quda

---
+++ Res/Scripts/AntiAd/quda.js
@@ -0,0 +1,18 @@
+/********************************
+QuDa Remove Ads - Version 1.0
+Please note that you may need to reinstall app for script to work.
+
+QuantumultX rewrite link:
+https://raw.githubusercontent.com/zirawell/R-Store/main/Rule/QuanX/Adblock/App/Q/去上网（去哒）/rewrite/iQuShangWang.conf
+
+********************************/
+
+if (!$response.body) $done({});
+var body = $response.body;
+var obj = JSON.parse(body);
+if (obj.data.length == 600) {
+    obj.data = "null";
+    $done({body: JSON.stringify(obj)});
+} else {
+    $done({});
+}
