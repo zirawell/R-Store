6dcd3eb4f0742e21cd7a94cebafe260aa51f65d6
Dark Wang <zirawell@gmail.com>
2024-10-22 10:47:07 +0800

    upd ltsst

---
+++ Res/Scripts/AntiAd/ltsst.js
@@ -0,0 +1,21 @@
+/********************************
+Ltsst Remove Ads - Version 1.0
+Please note that you may need to reinstall app for script to work.
+
+QuantumultX rewrite link:
+https://raw.githubusercontent.com/zirawell/R-Store/main/Rule/QuanX/Adblock/App/L/旅途随身听/rewrite/1314zhilv.conf
+
+********************************/
+const url = $request.url;
+if (!$response.body) $done({});
+let body = $response.body;
+let obj = JSON.parse(body);
+
+if (url.includes('/common/getJGQIconNew')) {
+  delete obj.content.specialBanner;
+} else if (url.includes('/city/getAllBannelByCity')) {
+  obj.content = obj.content.filter(i => i.bannerType === 1);
+}
+
+body = JSON.stringify(obj);
+$done({ body });
