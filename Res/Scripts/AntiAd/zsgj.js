70f735489f08dfcb7d1c07f60208b60d5232a889
Dark Wang <zirawell@gmail.com>
2024-10-22 16:34:54 +0800

    upd zsgj

---
+++ Res/Scripts/AntiAd/zsgj.js
@@ -0,0 +1,13 @@
+/********************************
+ZSGJ Remove Ads - Version 1.0
+Checkout Source - https://raw.githubusercontent.com/fmz200/wool_scripts/main/Scripts/youdao/dict-youdao-ad.js
+Please note that you may need to reinstall app for script to work.
+
+QuantumultX rewrite link:
+https://raw.githubusercontent.com/zirawell/R-Store/main/Rule/QuanX/Adblock/Applet/Wechat/Z/掌上公交/rewrite/mygolbs.conf
+
+********************************/
+
+if (!$response.body) $done({});
+var body = $response.body.replace(/Ad":1/g, 'Ad":0').replace(/Ad_ab":1/g, 'Ad_ab":0')
+$done({body});
