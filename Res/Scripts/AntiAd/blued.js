dcfadeb60869a94ee5ae3f470debd3966bc7c63a
Dark Wang <zirawell@gmail.com>
2024-10-22 17:13:09 +0800

    upd blued

---
+++ Res/Scripts/AntiAd/blued.js
@@ -0,0 +1,18 @@
+/********************************
+Blued Remove Ads - Version 1.0
+Please note that you may need to reinstall app for script to work.
+
+QuantumultX rewrite link:
+https://raw.githubusercontent.com/zirawell/R-Store/main/Rule/QuanX/Adblock/App/B/Blued/rewrite/blued.conf
+
+********************************/
+
+if (!$response.body) $done({});
+let body = $response.body;
+let obj = JSON.parse(body);
+obj.data[0].banner = {};
+obj.data[0].service = [];
+obj.data[0].healthy = {};
+obj.data[0].healthy_banner = [];
+obj.data[0].emotions = [];
+$done({body: JSON.stringify(obj)});
