8066abcd4343911ea6ede5f3219e586fea814044
Dark Wang <zirawell@gmail.com>
2024-10-22 09:39:30 +0800

    upd mishop

---
+++ Res/Scripts/AntiAd/mishop.js
@@ -0,0 +1,18 @@
+/********************************
+Mishop Remove Ads - Version 1.0
+Please note that you may need to reinstall app for script to work.
+
+QuantumultX rewrite link:
+https://raw.githubusercontent.com/zirawell/R-Store/main/Rule/QuanX/Adblock/App/X/小米商城/rewrite/mishop.conf
+
+********************************/
+
+const url = $request.url;
+if (!$response.body) $done({});
+let body = $response.body;
+let obj = JSON.parse(body);
+if (obj?.data?.bottom?.ad_info) {
+  delete obj.data.bottom.ad_info;
+}
+body = JSON.stringify(obj);
+$done({ body });
