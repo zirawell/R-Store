875ca954d50f936bcacbe2603e17320de728bd98
Dark Wang <zirawell@gmail.com>
2024-10-23 13:12:38 +0800

    upd mgtv

---
+++ Res/Scripts/AntiAd/mgtv.js
@@ -0,0 +1,25 @@
+/********************************
+XiaoMang Remove Ads - Version 1.0
+Checkout Source - https://raw.githubusercontent.com/Sliverkiss/QuantumultX/main/AdBlock/xmApp/xmApp.js
+Please note that you may need to reinstall app for script to work.
+
+QuantumultX rewrite link:
+https://raw.githubusercontent.com/zirawell/R-Store/main/Rule/QuanX/Adblock/App/X/小芒/rewrite/mgtv.conf
+
+********************************/
+
+const url = $request.url;
+if (!$response.body) $done({});
+let body = $response.body;
+let Body = JSON.parse(body);
+if (url.match(/icon/)) {
+    Body.data = {
+        ...Body.data,
+        big_promotion_center: {},
+        active_center: {}
+    }
+} else if(url.match(/index/)){
+    let filtData=Body.data.filter(e=>e.hasMore);
+    Body.data = filtData;
+}
+$done({ body: JSON.stringify(Body) })
