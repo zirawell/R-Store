419910727289e03b466490df5ae4f0e2ed4af4b2
Dark Wang <zirawell@gmail.com>
2024-10-23 20:03:25 +0800

    upd peiyinxiu

---
+++ Res/Scripts/AntiAd/dubbingShow.js
@@ -0,0 +1,41 @@
+/********************************
+DubbingShow Remove Ads - Version 1.0 
+Date - 2024-08-28 08:57:11
+Checkout Source - https://gitlab.com/lodepuly/vpn_tool/-/raw/master/Resource/Script/DubbingShow/DubbingShow_remove_ads.js
+Please note that you may need to reinstall app for script to work.
+
+QuantumultX rewrite link:
+https://raw.githubusercontent.com/zirawell/R-Store/main/Rule/QuanX/Adblock/App/P/配音秀/rewrite/peiyinxiu.conf
+
+********************************/
+
+
+const url = $request.url;
+const body = $response.body;
+
+if (!body) $done({});
+
+function removeDataWithKeyword(array) {
+    if (Array.isArray(array)) {
+        for (let i = array.length - 1; i >= 0; i--) {
+            if (array[i].data && array[i].data.includes("金币哦")) {
+                array.splice(i, 1);
+            }
+        }
+    }
+}
+
+try {
+    let obj = JSON.parse(body);
+
+    if (url.includes("/Api/Film/GetConfigValue")) {
+        if (obj.data && Array.isArray(obj.data)) {
+            removeDataWithKeyword(obj.data);
+        }
+    }
+
+    $done({ body: JSON.stringify(obj) });
+} catch (e) {
+    console.error("JSON parsing error:", e);
+    $done({});
+}
