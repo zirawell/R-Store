0c16fb3090bdc9510192c7ec2d104cb4baeca2c6
Dark Wang <zirawell@gmail.com>
2024-12-27 17:31:40 +0800

    upd dianping

--- Res/Scripts/AntiAd/dianping.js
+++ Res/Scripts/AntiAd/dianping.js
@@ -1,6 +1,7 @@
 /********************************
 Dianping Remove Ads - Version 1.0
 Please note that you may need to reinstall app for script to work.
+Works for version 11.28.3 and below. Version ID: 870198258
 
 QuantumultX rewrite link:
 https://raw.githubusercontent.com/zirawell/R-Store/main/Rule/QuanX/Adblock/App/D/大众点评/rewrite/dianPing.conf
@@ -8,13 +9,20 @@ https://raw.githubusercontent.com/zirawell/R-Store/main/Rule/QuanX/Adblock/App/D
 ********************************/
 
 const url = $request.url;
-const header = $request.headers;
-const resp = {};
-const traceKey = Object.keys(header).find(key => /^m-(shark-)?traceid$/i.test(key));
-const headopt = traceKey ? header[traceKey] : null;
-
-if (headopt) {
-  $done({ body:"", headers:"", status: "HTTP/1.1 204 No Content" });
-} else{
-  $done({});
+if(url.includes("/dpmobile")){
+  const header = $request.headers;
+  const resp = {};
+  const traceKey = Object.keys(header).find(key => /^m-(shark-)?traceid$/i.test(key));
+  const headopt = traceKey ? header[traceKey] : null;
+  
+  if (headopt) {
+    $done({ body:"", headers:"", status: "HTTP/1.1 204 No Content" });
+  } else{
+    $done({});
+  }
+}else if(url.includes("/picassovc")){
+  if (!$response.body) $done({});
+  let body = $response.body;
+  body = body.replace(/function WedgetCard/g,'function WedgetCard0');
+  $done({ body });
 }
