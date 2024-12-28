ee5da19fedbaafaf9e47e9d69ad1dc69ea68eb05
Dark Wang <zirawell@gmail.com>
2024-12-28 17:27:33 +0800

    upd usmile

---
+++ Res/Scripts/AntiAd/usmile.js
@@ -0,0 +1,20 @@
+/********************************
+Usmile Remove Ads - Version 1.0
+Please note that you may need to reinstall app for script to work.
+
+QuantumultX rewrite link:
+https://raw.githubusercontent.com/zirawell/R-Store/main/Rule/QuanX/Adblock/App/X/笑容加/rewrite/usmile.conf
+
+********************************/
+
+const url = $request.url;
+const header = $request.headers;
+const resp = {};
+const traceKey = Object.keys(header).find(key => /^tok/i.test(key));
+const headopt = traceKey ? header[traceKey] : null;
+
+if (headopt) {
+  $done({ body:"", headers:"", status: "HTTP/1.1 204 No Content" });
+} else{
+  $done({});
+}
