42db5f0de9ffbac90693cb740efb97b6607c62e3
Dark Wang <zirawell@gmail.com>
2025-04-21 17:15:50 +0800

    upd ccbLife

--- Res/Scripts/AntiAd/ccbLife.js
+++ Res/Scripts/AntiAd/ccbLife.js
@@ -11,8 +11,22 @@ const url = $request.url;
 if (!$response.body) $done({});
 let body = $response.body;
 let obj = JSON.parse(body);
-if (obj?.data?.ICON_SKIN_INFO) {
-  obj.data.ICON_SKIN_INFO = {};
+if (url.includes("A3341AB04")) {
+  if (obj?.data?.ICON_SKIN_INFO) {
+    delete obj.data.ICON_SKIN_INFO;
+  }
+}else if (url.includes("A3341AB03")) {
+  if (obj?.data?.TAG_AD_INFO){
+    delete obj.data.TAG_AD_INFO;
+  }
+  if (obj?.data?.MEBCT_AD_INFO){
+    delete obj.data.MEBCT_AD_INFO;
+  }
+}else if(url.includes("A3341A120")) {
+  if(obj?.data?.POP_AD_INFO){
+    delete obj.data.POP_AD_INFO;
+  }
 }
+
 body = JSON.stringify(obj);
 $done({ body });
