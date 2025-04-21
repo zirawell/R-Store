631444997cec64cab72a5f799f60913d5b92176f
Dark Wang <zirawell@gmail.com>
2025-04-21 17:37:25 +0800

    upd ref

--- Res/Scripts/AntiAd/ccbLife.js
+++ Res/Scripts/AntiAd/ccbLife.js
@@ -3,7 +3,7 @@ CCBLife Remove Ads - Version 1.0
 Please note that you may need to reinstall app for script to work.
 
 QuantumultX rewrite link:
-https://raw.githubusercontent.com/zirawell/R-Store/main/Rule/QuanX/Adblock/App/J/建行生活/rewrite/yunBusiness.conf
+https://raw.githubusercontent.com/zirawell/R-Store/main/Rule/QuanX/Adblock/App/J/建行生活/rewrite/ccbLife.conf
 
 ********************************/
 
