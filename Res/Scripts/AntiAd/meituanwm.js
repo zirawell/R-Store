5dc0b24d948eef6b8242a7ecfe18b9983a4106e3
Dark Wang <zirawell@gmail.com>
2025-04-30 10:44:12 +0800

    upd meituanwm

---
+++ Res/Scripts/AntiAd/meituanwm.js
@@ -0,0 +1,32 @@
+/********************************
+MeituanWM Remove Ads - Version 1.0
+Checkout Source - https://raw.githubusercontent.com/ZenmoFeiShi/Qx/refs/heads/main/MeiTuanNoAd.js
+Please note that you may need to reinstall app for script to work.
+
+QuantumultX rewrite link:
+https://raw.githubusercontent.com/zirawell/R-Store/main/Rule/QuanX/Adblock/Applet/Wechat/M/美团外卖/rewrite/meituanwm.conf
+
+********************************/
+
+const url = $request.url;
+if (!$response.body) $done({});
+let obj = JSON.parse($response.body);
+
+// 微信小程序
+if (url.includes("api/miniprogram")) {
+    if (obj?.data) {
+        if (Array.isArray(obj.data)) {
+            obj.data = obj.data.filter(item => {
+                if (item && item.name) {
+                    return ["首页", "神券", "我的"].includes(item.name);
+                }
+                return false;
+            });
+        } else {
+            if (obj.data.oneCentBuy) {
+                delete obj.data.oneCentBuy;
+            }
+        }
+    }
+}
+$done({body: JSON.stringify(obj)});
