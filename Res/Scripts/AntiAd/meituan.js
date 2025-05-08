e2548fc81fbd48f34940d8e86f54a1d1dc1094f5
Dark Wang <zirawell@gmail.com>
2025-05-08 15:50:02 +0800

    upd meituan

---
+++ Res/Scripts/AntiAd/meituan.js
@@ -0,0 +1,31 @@
+/********************************
+Meituan Remove Ads - Version 1.0
+Please note that you may need to reinstall app for script to work.
+
+QuantumultX rewrite link:
+https://raw.githubusercontent.com/zirawell/R-Store/main/Rule/QuanX/Adblock/App/M/美团/rewrite/meituan.conf
+
+********************************/
+
+const url = $request.url;
+const iconRegex1 = /\.(gif|jpg|webp|png)@(100w_100h|150w_150h|132w_96h|1284w|1200w_0h)/;
+const iconRegex2 = /%(4088h_88w|40115h_115w)_1e_1l\.(gif|jpg|webp|png)/;
+const urlRegex1 = /https?:\/\/p\d\.meituan\.net\/linglong\/\w+\.(gif|jpg|webp|png)$/;
+var removeFlag = true;
+if (url.includes("/linglong")) {
+    if (urlRegex1.test(url)) {
+        const header = $request.headers;
+        const traceKey = Object.keys(header).find(key => /^ai|dt|al|u/i.test(key));
+        removeFlag = traceKey ? true : false;
+    } else {
+        if (url.match(iconRegex1) || url.match(iconRegex2)) {
+            removeFlag = false;
+        }
+    }
+    
+    if (removeFlag) {
+        $done({body: "", headers: "", status: "HTTP/1.1 204 No Content"});
+    } else {
+        $done({});
+    }
+}
