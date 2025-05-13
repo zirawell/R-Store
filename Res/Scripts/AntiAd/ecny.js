a0efddc2374bc19501752f2351b64ae7bcb636b3
Dark Wang <zirawell@gmail.com>
2025-05-13 15:39:43 +0800

    upd ecny

--- Res/Scripts/AntiAd/ecny.js
+++ Res/Scripts/AntiAd/ecny.js
@@ -3,7 +3,7 @@ eCNY Remove Ads - Version 1.0
 Please note that you may need to reinstall app for script to work.
 
 QuantumultX rewrite link:
-https://raw.githubusercontent.com/zirawell/R-Store/main/Rule/QuanX/Adblock/App/S/数字人民币/rewrite/eCNY.conf
+https://raw.githubusercontent.com/zirawell/R-Store/main/Rule/QuanX/Adblock/App/S/数字人民币/rewrite/ecny.conf
 
 ********************************/
 
