9f6331a7589cb2c2fe1a57759c4286e48679a4ef
Dark Wang <zirawell@gmail.com>
2025-05-13 15:34:20 +0800

    upd mdb

--- Res/Scripts/AntiAd/mdb.js
+++ Res/Scripts/AntiAd/mdb.js
@@ -3,7 +3,7 @@ Maidanba Remove Ads - Version 1.0
 Please note that you may need to reinstall app for script to work.
 
 QuantumultX rewrite link:
-https://raw.githubusercontent.com/zirawell/R-Store/main/Rule/QuanX/Adblock/App/M/买单吧/rewrite/creditCardBankComm.conf
+https://raw.githubusercontent.com/zirawell/R-Store/main/Rule/QuanX/Adblock/App/M/买单吧/rewrite/mdb.conf
 
 ********************************/
 
