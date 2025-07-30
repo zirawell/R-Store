04e9894745b63a6f82219f31b2181709698b97a2
Dark Wang <zirawell@gmail.com>
2025-07-30 10:35:15 +0800

    upd sth

--- Res/Scripts/Unlock/bybutter.js
+++ Res/Scripts/Unlock/bybutter.js
@@ -3,8 +3,21 @@ ButterCam Unlock Vip - Version 1.0
 Checkout Source - https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/hy.js
 Please note that you may need to reinstall app for script to work.
 
-QuantumultX rewrite link:
-https://raw.githubusercontent.com/zirawell/R-Store/main/Rule/QuanX/Adblock/App/H/黄油相机/rewrite/bybutter.conf
+- QuantumultX:
+
+[rewrite_local]
+^https?:\/\/api\d\.bybutter\.com\/v\d url script-response-body https://raw.githubusercontent.com/zirawell/R-Store/main/Res/Scripts/Unlock/bybutter.js
+
+[mitm]
+hostname = api*.bybutter.com
+
+- Surge:
+
+[Script]
+bybutter = type=http-response,pattern=^https?:\/\/api\d\.bybutter\.com\/v\d,requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/zirawell/R-Store/main/Res/Scripts/Unlock/bybutter.js
+
+[MITM]
+hostname= %APPEND% api*.bybutter.com
 
 ********************************/
 
