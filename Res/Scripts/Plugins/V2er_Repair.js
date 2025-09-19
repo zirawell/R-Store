a19a079bf4504738e224821fecaf2f5ca1b90a3b
Dark Wang <zirawell@gmail.com>
2025-09-19 10:51:34 +0800

    upd v2er

--- Res/Scripts/Plugins/V2er_Repair.js
+++ Res/Scripts/Plugins/V2er_Repair.js
@@ -8,19 +8,32 @@ https://raw.githubusercontent.com/zirawell/R-Store/refs/heads/main/Rule/QuanX/Pl
 ********************************/
 
 const resp = {};
+const url = $request.url;
 if (!$response.body) $done({});
 var parser = new DOMParser();
 var doc = parser.parseFromString($response.body, "text/html");
-const flexDiv = doc.querySelector('div.flex-one-row.gap10');
-if (flexDiv) {
-    const h1Element = flexDiv.querySelector('h1');
-    const parentTd = flexDiv.closest('td');
-    if (h1Element && parentTd) {
-        parentTd.insertBefore(h1Element, flexDiv);
-        if (flexDiv.children.length === 0 && flexDiv.textContent.trim() === '') {
-            flexDiv.remove();
-        }
-    }
+
+if (url.includes("/member")) {
+  const flexDiv = doc.querySelector('div.flex-one-row.gap10');
+  if (flexDiv) {
+      const h1Element = flexDiv.querySelector('h1');
+      const parentTd = flexDiv.closest('td');
+      if (h1Element && parentTd) {
+          parentTd.insertBefore(h1Element, flexDiv);
+          if (flexDiv.children.length === 0 && flexDiv.textContent.trim() === '') {
+              flexDiv.remove();
+          }
+      }
+  }
+} else if (url.includes("/mission/daily")) {
+  const cellWithLoginInfo = Array.from(doc.querySelectorAll('.cell'))
+  .find(cell => cell.textContent.includes('已连续登录'));
+  if (cellWithLoginInfo) {
+      const frDiv = cellWithLoginInfo.querySelector('.fr');
+      if (frDiv) {
+          frDiv.remove();
+      }
+  }
 }
 resp.body = doc.documentElement.outerHTML;
 $done(resp);
