7b6b0958a120c95267d9c7c96cbb7671ab5ba896
Dark Wang <zirawell@gmail.com>
2025-09-25 16:57:35 +0800

    upd dianping

--- Res/Scripts/AntiAd/dianping.js
+++ Res/Scripts/AntiAd/dianping.js
@@ -18,6 +18,14 @@ if (url.includes("/dpmobile") || url.includes("/goodsawardpic")) {
   const headopt2 = traceKey2 ? header[traceKey2] : null;
   if (headopt1 && !headopt2) {
     $done({body: "", headers: "", status: "HTTP/1.1 404 Not Found"});
+  } else if (url.includes(".gif")) {
+    console.log("url" + url);
+    const hexString = "47494638396101000100800000000000ffffff21f90401000000002c000000000100010000020144003b";
+    const header = {};
+    header["Content-Type"] = "image/gif";
+    header["Content-length"] = 42;
+    header["Connection"] = "close";
+    $done({bodyBytes: hexStringToArrayBuffer(hexString),headers: header, status: "HTTP/1.1 200 OK"});
   } else {
     $done({});
   }
@@ -35,4 +43,17 @@ function debug(url,traceKey1,traceKey2,headopt1,headopt2) {
   console.log("traceKey2:" + traceKey2);
   console.log("headopt1:" + headopt1);
   console.log("headopt2:" + headopt2);
+}
+
+function hexStringToArrayBuffer(hexString) {
+  const cleanHex = hexString.replace(/[^0-9A-Fa-f]/g, '');
+  if (cleanHex.length % 2 !== 0) {
+    throw new Error('十六进制字符串长度必须是偶数');
+  }
+  const buffer = new ArrayBuffer(cleanHex.length / 2);
+  const view = new Uint8Array(buffer);
+  for (let i = 0; i < cleanHex.length; i += 2) {
+      view[i / 2] = parseInt(cleanHex.substr(i, 2), 16);
+  }
+  return buffer;
 }
