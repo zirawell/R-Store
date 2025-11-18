8f61b241f118846547d900776b0e7fd2332cb551
Dark Wang <zirawell@gmail.com>
2025-11-18 16:09:13 +0800

    opt meituan

--- Res/Scripts/AntiAd/meituan.js
+++ Res/Scripts/AntiAd/meituan.js
@@ -10,16 +10,20 @@ https://raw.githubusercontent.com/zirawell/R-Store/main/Rule/Surge/Adblock/App/M
 ********************************/
 
 const url = $request.url;
-const iconRegex1 = /\.(gif|jpg|webp|png)@(100w_100h|120w_120h|150w_150h|250w_250h|132w_96h|186w_135h|1284w|250w_0h|400w_0h|700w_0h|1200w_0h)/;
+const iconRegex1 = /\.(gif|jpg|webp|png)@(132w_96h|186w_135h|1284w|250w_0h|400w_0h|700w_0h|1200w_0h)/;
 const iconRegex2 = /%(4088h_88w|40115h_115w|4047h_189w|40157h_189w)_1e_1l\.(gif|jpg|webp|png)/;
-const urlRegex1 = /https?:\/\/p\d\.meituan\.net\/linglong\/\w+\.(gif|jpg|webp|png)$/;
+const urlRegex = /https?:\/\/p\d\.meituan\.net\/linglong\/\w+\.(gif|jpg|webp|png)$/;
 var removeFlag = true;
 if (url.includes("/linglong")) {
-    if (urlRegex1.test(url)) {
+    if (urlRegex.test(url)) {
         const header = $request.headers;
-        const traceKey = Object.keys(header).find(key => /^ai|dt|al|u/i.test(key));
+        const traceKey = Object.keys(header).find(key => /^(ai|dt|al|u)$/i.test(key));
         removeFlag = traceKey ? true : false;
     } else {
+        const dimStr = extractAfterAt(url);
+        if ("-1" != dimStr && compareDimensions(dimStr)) {
+            removeFlag = false;
+        }
         if (url.match(iconRegex1) || url.match(iconRegex2)) {
             removeFlag = false;
         }
@@ -30,4 +34,24 @@ if (url.includes("/linglong")) {
     } else {
         $done({});
     }
+}
+
+
+function extractAfterAt(str) {
+    const atIndex = str.lastIndexOf("@");
+    if (atIndex === -1) {
+        return "-1";
+    }
+    return str.substring(atIndex + 1);
+}
+
+function compareDimensions(str) {
+    const widthMatch = str.match(/(\d+)w/);
+    const width = widthMatch ? parseInt(widthMatch[1]) : null;
+    const heightMatch = str.match(/(\d+)h/);
+    const height = heightMatch ? parseInt(heightMatch[1]) : null;
+    if (width == null || height == null) {
+        return false;
+    }
+    return width == height || Math.abs(width - height) === 1;;
 }
